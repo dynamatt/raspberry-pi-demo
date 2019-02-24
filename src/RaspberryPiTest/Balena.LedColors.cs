2e4bf0147c43d1cbd29286f0ce512d8e19d61de4
Unknown <mattmwilliams89@gmail.com>
2019-02-25 07:35:06 +1100

    Use process to change RGB led

---
+++ src/RaspberryPiTest/Balena.LedColors.cs
@@ -0,0 +1,18 @@
+﻿namespace Balena
+{
+    public class LedColor
+    { 
+        public bool Red { get; internal set; }
+        public bool Green { get; internal set; }
+        public bool Blue { get; internal set; }
+    }
+
+    public class LedColors
+    {
+        public static readonly LedColor Red = new LedColor { Red = true };
+        public static readonly LedColor Yellow = new LedColor { Red = true, Green = true };
+        public static readonly LedColor Green = new LedColor { Green = true };
+        public static readonly LedColor Blue = new LedColor { Blue = true };
+        public static readonly LedColor Purple = new LedColor { Red = true, Blue = true };
+    }
+}
--- src/RaspberryPiTest/IHomeLoggerHardware.cs
+++ src/RaspberryPiTest/IHomeLoggerHardware.cs
@@ -1,6 +1,7 @@
 ﻿namespace RaspberryPiTest
 {
     using System;
+    using System.Diagnostics;
     using System.Reactive.Linq;
     using Unosquare.RaspberryIO;
     using Unosquare.RaspberryIO.Abstractions;
@@ -39,21 +40,41 @@
         private void InitialisePins()
         {
             Pi.Init<Unosquare.WiringPi.BootstrapWiringPi>();
-            Pi.Gpio[504].PinMode = GpioPinDriveMode.Output;
-            Pi.Gpio[505].PinMode = GpioPinDriveMode.Output;
-            Pi.Gpio[506].PinMode = GpioPinDriveMode.Output;
+            //Pi.Gpio[504].PinMode = GpioPinDriveMode.Output;
+            //Pi.Gpio[505].PinMode = GpioPinDriveMode.Output;
+            //Pi.Gpio[506].PinMode = GpioPinDriveMode.Output;
+
+            var process = new Process();
+            process.StartInfo.FileName =
+            "echo 504 > /sys/class/gpio/export # (Red)\n" +
+            "echo 505 > /sys/class/gpio/export # (Green)\n" +
+            "echo 506 > /sys/class/gpio/export # (Blue)\n" +
+            "echo \"out\" > /sys/class/gpio/gpio504/direction\n" +
+            "echo \"out\" > /sys/class/gpio/gpio505/direction\n" +
+            "echo \"out\" > /sys/class/gpio/gpio506/direction\n";
+            process.Start();
         }
 
         public override string ToString()
         {
             return Pi.Info.ToString();
         }
+
         private bool isOn = false;
         public void SetLed(double red, double green, double blue)
         {
-            Pi.Gpio[504].Write(!isOn);
-            Pi.Gpio[505].Write(!isOn);
-            Pi.Gpio[506].Write(!isOn);
+            //Pi.Gpio[504].Write(!isOn);
+            //Pi.Gpio[505].Write(!isOn);
+            //Pi.Gpio[506].Write(!isOn);
+            var process = new Process();
+            int r = red > 0 ? 1 : 0;
+            int g = green > 0 ? 1 : 0;
+            int b = blue > 0 ? 1 : 0;
+            process.StartInfo.FileName =
+            $"echo {r} > / sys /class/gpio/gpio504/value\n" +
+            $"echo {g} > /sys/class/gpio/gpio505/value\n" +
+            $"echo {b} > /sys/class/gpio/gpio506/value";
+            process.Start();
             isOn = !isOn;
         }
     }
